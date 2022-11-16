82e702916892e2fda38a19237f01be2434f5f1af
Edmund Sagoe <edsagoefvr@gmail>
2022-11-16 19:11:54 +0000

    path

--- path.c
+++ path.c
@@ -1,177 +1,83 @@
 #include "shell.h"
 
 /**
- * path_finder - Acts as an interface for functions that will be able to
- * find the full path of a program.
- * @command: Represents a command. For example ls, echo, pwd, etc.
- * Return: Upon sucess a string with the full path of the program.
- * for example /bin/ls, /bin/echo, etc. Otherwise returns NULL.
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ * Return: 1 if true, 0 otherwise
  */
-char *path_finder(char *command)
+int is_cmd(info_t *info, char *path)
 {
-	char *str = "PATH";
-	char *constructed;
-	char **path_tokens;
-	int index;
-	char *directory;
+	struct stat st;
 
-	index = find_path(str);
-	path_tokens = tokenize_path(index, str);
-	if (path_tokens == NULL)
-		return (NULL);
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
 
-	directory = search_directories(path_tokens, command);
-	if (directory == NULL)
+	if (st.st_mode & S_IFREG)
 	{
-		double_free(path_tokens);
-		return (NULL);
+		return (1);
 	}
-
-	constructed = build_path(directory, command);
-	if (constructed == NULL)
-	{
-		double_free(path_tokens);
-		return (NULL);
-	}
-
-	double_free(path_tokens);
-
-	return (constructed);
+	return (0);
 }
-/**
- * find_path - Finds the index of an environmental variable.
- * @str: Environmental variable that needs to be found.
- * Return: Upon success returns the index of the environmental variable.
- * otherwise returns -1.
- */
-int find_path(char *str)
-{
-	int i;
-	int len;
-	int j;
 
-	len = _strlen(str);
-	for (i = 0; environ[i] != NULL; i++)
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buffer[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buffer[k++] = pathstr[i];
+	buffer[k] = 0;
+	return (buffer);
+}
+
+/**
+ * find_path - finds this cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ * Return: full path of cmd if found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
 	{
-		for (j = 0; j < len; j++)
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
 		{
-			if (environ[i][j] != str[j])
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
 				break;
+			curr_pos = i;
 		}
-		if (j == len && environ[i][j] == '=')
-			return (i);
-	}
-	return (-1);
-}
-
-/**
- * tokenize_path - Separates a string representing paths as an array
- * of strings contining the path directories.
- * @index: Index of the path in the environment variables.
- * @str: string to separate and tokenize.
- * Return: Upon success a NULL terminated array of pointer to strings.
- * Otherwise returns NULL. Note!: Do not forget to free alocated
- * memory on receiving function or when possible.
- */
-char **tokenize_path(int index, char *str)
-{
-	char *env_var;
-	int token_count;
-	const char *delim = ":\n";
-	char **path_tokens;
-	int len;
-
-	len = _strlen(str);
-	token_count = 0;
-	/*Moving the pointer len of str plus = sign*/
-	env_var = environ[index] + (len + 1);
-	path_tokens = token_interface(env_var, delim, token_count);
-	if (path_tokens == NULL)
-		return (NULL);
-
-	return (path_tokens);
-}
-
-/**
- * search_directories - Looks through directories stored in path_tokens for a
- * specific file. aka commmand.
- * @path_tokens: A pointer to an array of strings representing the different
- * paths contained in the PATH environmental varible.
- * @command: Represents a command. For example ls, echo, pwd, /bin/ls etc.
- * Return: Upon success a string with the upper most directory containing
- * the command file. Otherwise returns NULL.
- */
-char *search_directories(char **path_tokens, char *command)
-{
-	int i, s;
-	char *cwd;
-	char *buf;
-	size_t size;
-	struct stat stat_buf;
-
-	buf = NULL;
-	size = 0;
-	cwd = getcwd(buf, size);
-	if (cwd == NULL)
-		return (NULL);
-	if (command[0] == '/')
-		command = command + 1;
-	for (i = 0; path_tokens[i] != NULL; i++)
-	{
-		s = chdir(path_tokens[i]);
-		if (s == -1)
-		{
-			perror("ERROR!");
-			return (NULL);
-		}
-		s = stat(command, &stat_buf);
-		if (s == 0)
-		{
-			chdir(cwd);
-			free(cwd);
-			return (path_tokens[i]);
-		}
-	}
-	chdir(cwd);
-	free(cwd);
-	return (NULL);
-}
-
-/**
- * build_path - Combines two strings one representing the path directory and
- * one representing the command file.
- * @directory: Represents a directory in the path.
- * @command: Represents a file in a directory of the path.
- * Return: Upon success a string representing the full path of a command.
- * Otherwise NULL.
- */
-char *build_path(char *directory, char *command)
-{
-	int i, j;
-	int dir_len;
-	int command_len;
-	int len;
-	char *built;
-
-	if (directory == NULL || command == NULL)
-		return (NULL);
-	dir_len = _strlen(directory) + 1;
-	command_len = _strlen(command) + 1;
-	len = dir_len + command_len;
-
-	built = malloc(sizeof(char) * len);
-	if (built == NULL)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
-	{
-		for (j = 0; directory[j] != '\0'; j++, i++)
-			built[i] = directory[j];
-		built[i] = '/';
 		i++;
-		for (j = 0; command[j] != '\0'; j++, i++)
-			built[i] = command[j];
 	}
-	built[--i] = '\0';
-	return (built);
+	return (NULL);
 }
