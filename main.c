46f64c64d09cf128ed2d7cd42eaba5e3102da55f
Edmund Sagoe <edsagoefvr@gmail>
2022-11-16 19:17:07 +0000

    main

--- main.c
+++ main.c
@@ -1,27 +1,43 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the program.
- * @ac: The number of parameters passed to the executable file. In the case
- * this variable will not be used.
- * @av: The name of the program.
- * Return: Always 0.
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ * Return: 0 onn success, 1 on error
  */
-int main(__attribute__((unused)) int ac, char **av)
+int main(int ac, char **av)
 {
-	char *line;
-	size_t size;
-	int command_counter;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	command_counter = 0;
-	signal(SIGINT, SIG_IGN);
-	do {
-		command_counter++;
-		line = NULL;
-		size = 0;
-		parse_line(line, size, command_counter, av);
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-	} while (1);
-
-	return (0);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
